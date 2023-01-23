2b4674ae8e7c14c8d2547f4dbfb38f25be42c1d3
android studio <androidstudio@iMac-Administrator.local>
2023-01-23 13:03:41 +0900

    Инициализация

---
+++ app/src/main/java/com/example/myapplication/GitHubService.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+interface GitHubService {
+    // GET /repos/:owner/:repo/contributors
+
+    @GET("repos/{owner}/{repo}/contributors")
+    Call<List<Contributor>> repoContributors(
+            @Path("owner") String owner,
+            @Path("repo") String repo);
+
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+}
